0c11ee44d63ab97174c4174077fc9bfb47c5e321
Debosmita Roy <debosmita456@gmail.com>
2023-12-23 14:49:41 +0530

    broadcast changes b/w clients

--- client/src/TextEditor.js
+++ client/src/TextEditor.js
@@ -1,6 +1,7 @@
-import { useCallback } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import Quill from "quill"
 import "quill/dist/quill.snow.css"
+import {io} from 'socket.io-client'
 
 const TOOLBAR_OPTIONS = [
     [{ header: [1, 2, 3, 4, 5, 6, false] }],
@@ -16,7 +17,42 @@ const TOOLBAR_OPTIONS = [
 
 
 export default function TextEditor() {
+    const[socket, setSocket]= useState()
+    const [quill, setQuill] = useState()
     // const wrapperRef = useRef()
+    useEffect(()=>{
+        const s = io("http://localhost:3001")
+        setSocket(s)
+        return() => {
+            s.disconnect()
+        }
+    },[])
+
+    useEffect(()=>{
+        //Update the document with changes(delta) from our other client
+        if (socket == null || quill == null) return  // Initially quill and socket are going to be null hence we don't want to do anything unless until the connection is setup.
+        const handler = (delta) => {
+            quill.updateContents(delta)
+        }
+        socket.on('receive-changes', handler)
+
+        return() => {
+            socket.off('receive-changes', handler)
+        }
+    }, [socket,quill])
+
+    useEffect(()=>{
+        if (socket == null || quill == null) return  // Initially quill and socket are going to be null hence we don't want to do anything unless until the connection is setup.
+        const handler = (delta, oldDelta, source) => {
+            if (source !== 'user') return //only track changes if user made them
+            socket.emit("send-changes", delta) // delta is not the whole document. It is only a subset of the document that is changing.
+        }
+        quill.on('text-change', handler)
+
+        return() => {
+            quill.off('text-changes', handler)
+        }
+    }, [socket,quill])
 
     const wrapperRef= useCallback((wrapper)=>{
         if (wrapper == null) return
@@ -24,8 +60,8 @@ export default function TextEditor() {
         wrapper.innerHTML = ""
         const editor = document.createElement("div")
         wrapper.append(editor)
-        new Quill(editor, {theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS} })
-        
+        const q = new Quill(editor, {theme: "snow", modules: {toolbar: TOOLBAR_OPTIONS} })
+        setQuill(q)
 
     }, [])
     return <div className="container" ref={wrapperRef}></div>
