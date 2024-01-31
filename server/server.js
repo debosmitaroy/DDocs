dcaefeae461315d708b359e829010b9936988753
Debosmita Roy <debosmita456@gmail.com>
2024-01-31 11:04:50 +0530

    added db and socket logic

--- server/server.js
+++ server/server.js
@@ -1,14 +1,46 @@
-const io = require(`socket.io`)(3001, {
+
+require('dotenv').config()
+const mongoose = require("mongoose")
+const Document = require("./Document")
+
+const clientOptions = { serverApi: { version: '1', strict: true, deprecationErrors: true } };
+const uri = `mongodb+srv://${process.env.REACT_APP_MONGODB_USERNAME}:${process.env.REACT_APP_MONGODB_PASSWORD}@${process.env.REACT_APP_MONGODB_CLUSTER}.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(uri, clientOptions).then(()=> console.log('--DB Connected--'));
+// mongoose.connection.db.admin().command({ ping: 1 });
+
+
+const io = require('socket.io')(3001, {
     cors: {
         origin: 'http://localhost:3000',
         methods: ['GET', 'POST']
     }
 })
 
+const defaultValue = ""
+
 io.on("connection", socket => {
-    socket.on('send-changes', delta => {
-        // console.log(delta)
-        socket.broadcast.emit("receive-changes", delta)
-    })
-    
+    socket.on('get-document', async documentId => {
+        const document = await findOrCreateDocument(documentId)
+        socket.join(documentId)
+        const data = document.data? document.data: ''
+        socket.emit('load-document', data)
+        socket.on('send-changes', delta => {
+            socket.broadcast.to(documentId).emit("receive-changes", delta)
+        
+        })
+        //Saving the document
+        socket.on("save-document", async data => {
+            await Document.findByIdAndUpdate(documentId, {data})
+        })
+    })    
 })
+
+
+async function findOrCreateDocument(id) {
+    if(id==null) return
+
+    const document = await Document.findById(id)
+    if(document) return document
+    return await Document.create({_id: id, data: defaultValue}) 
+}
